24823889fd6de72c5ea21d7ed73a3c206deaec63
Vinicius Faria <artixsproductions@gmail.com>
2024-07-02 23:13:35 -0300

    fix: fixed imports

--- art_gallery.py
+++ art_gallery.py
@@ -1,9 +1,9 @@
-import src.util
-import src.algo
-import src.visual
+import util
+import algo
+import visual
 
 if __name__ == "__main__":
     num_points, points = util.readFile("instances-simple/simple-20-1.pol")
+    visual.plotPolygon(points)
     sol = algo.earclip(points)
     visual.plotTriangulation(points, sol)
-    visual.plotPolygon(points)
