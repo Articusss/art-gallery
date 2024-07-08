41707edd15acb47f10a3414c94f00bcfa0e6314c
Ramiro Noronha <ramironrribeiro@gmail.com>
2024-07-07 23:03:31 -0300

    Criação do voltar

--- teste.py
+++ teste.py
@@ -33,12 +33,87 @@ def cor_vertice(indice_cor):
         return 'red'
     return 'green'
 
+def show_triangles(fig):
+    global xs_triangulo
+    global ys_triangulo
+    for x,y in zip(xs_triangulo, ys_triangulo):
+        fig.add_trace(go.Scatter(x=x, y=y, mode="lines", line=dict(width=3)))
+
+def show_main_trace(fig, x, y):
+    main_trace = go.Scatter(x=x, y=y, mode="lines+markers", name="Main Trace", line=dict(color='blue', width=2))
+    fig.add_trace(main_trace)
+
+def show_current_ear_clip(fig, x_atual, y_atual, x_removido, y_removido):
+    indice_anterior, indice_proximo = encontra_indices_adjacentes(x_atual, x_removido)
+    x_traces = [x_atual[indice_anterior], x_removido, x_atual[indice_proximo]]
+    y_traces = [y_atual[indice_anterior], y_removido, y_atual[indice_proximo]]
+    traces = go.Scatter(x=x_traces, y=y_traces, mode="lines", line=dict(color='red', width=6))
+    fig.add_trace(traces)
+
+def show_removed_vertex(fig, x_removed, y_removed):
+    for x,y in zip(x_removed, y_removed):
+        fig.add_trace(go.Scatter(x=[x], y=[y], mode="markers", marker=dict(color=cor_vertice(vertices_cor[x_original.index(x)]), size=20))) 
+
+def get_x_vertex(state, list_states):
+    return [x[0] for x in list_states[state]] + [list_states[state][0][0]]
+
+def get_y_vertex(state, list_states):
+    return [y[1] for y in list_states[state]] + [list_states[state][0][1]]
+
+def update_triangles(triangulos, state):
+    triangle = triangulos[state -1]
+    sides = [points[x] for x in triangle ]
+    x_triangle = [x[0] for x in sides] + [sides[0][0]]
+    y_triangle = [x[1] for x in sides] + [sides[0][1]]
+
+    xs_triangulo.append(x_triangle)
+    ys_triangulo.append(y_triangle)
+
+def animation_get_ear_clip(fig, vertice_atual, estados_cortados):
+    x_atual = get_x_vertex(vertice_atual, estados_cortados)
+    y_atual = get_y_vertex(vertice_atual, estados_cortados)
+    vertice_seguinte = vertice_atual + 1 if vertice_atual < len(estados_cortados) -1  else 0
+    x_proximo = get_x_vertex(vertice_seguinte, estados_cortados)
+    y_proximo = get_y_vertex(vertice_seguinte, estados_cortados)
+            
+    show_main_trace(fig, x_atual, y_atual)
+
+    x_removido = encontra_elemento_removido(x_atual, x_proximo) if vertice_seguinte > 0 else x_atual[0]
+    y_removido = encontra_elemento_removido(y_atual, y_proximo) if vertice_seguinte > 0 else y_atual[0]
+    x_removidos.append(x_removido)
+    y_removidos.append(y_removido)
+    
+    
+    show_triangles(fig)
+    show_current_ear_clip(fig, x_atual, y_atual, x_removido, y_removido)
+    # show_removed_vertex(fig, x_removidos, y_removidos)
+
+def animation_new_graph_after_ear_clip(fig, vertice_atual, estados_cortados):
+    x_atual = [x[0] for x in estados_cortados[vertice_atual]] + [estados_cortados[vertice_atual][0][0]]
+    y_atual = [x[1] for x in estados_cortados[vertice_atual]] + [estados_cortados[vertice_atual][0][1]]
+            
+    update_triangles(triangulos, vertice_atual)
+    
+    show_main_trace(fig, x_atual, y_atual)
+    show_triangles(fig)
+
+def show_colors(fig, x_triangles, y_triangles):
+    for xs_triangle, ys_triangle in zip(x_triangles, y_triangles):
+        show_removed_vertex(fig, xs_triangle, ys_triangle)
+
+def animation_colors(fig, xs_triangulo, ys_triangulo, triangulo_atual):
+    xs_triangulo_atual = xs_triangulo[:triangulo_atual + 1]
+    ys_triangulo_atual = ys_triangulo[:triangulo_atual + 1]
+    show_triangles(fig)
+    show_colors(fig, xs_triangulo_atual, ys_triangulo_atual)
+
 num_points, points = util.read_file("instances-simple/simple-20-1.pol")
 # Dados de exemplo
 
 
 
 vertice_atual = 0
+triangulo_atual = 0
 
 triangulos, estados_cortados = novo.earclip(points)
 graph = util.build_graph_from_triangles(triangulos)
@@ -54,6 +129,9 @@ y_original = [x[1] for x in estados_cortados[0]] + [estados_cortados[0][0][1]]
 x_removidos = []
 y_removidos = []
 
+xs_triangulo = []
+ys_triangulo = []
+
 # Inicializa o aplicativo Dash
 app = dash.Dash(__name__)
 
@@ -61,14 +139,15 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
     dcc.Graph(id='live-graph', figure=inicia_o_grafico()),
     html.Button('Play', id='play-button', n_clicks=0),
-    
+    html.Button('Voltar', id='back-button', n_clicks=0),
 ])
 
 
 # Callback para atualizar o gráfico
 @app.callback(
-    Output('live-graph', 'figure'),
-    [Input('play-button', 'n_clicks')]
+    Output('live-graph', 'figure',allow_duplicate=True),
+    [Input('play-button', 'n_clicks')],
+    prevent_initial_call=True
 )
 def update_graph(n_clicks):
     global vertice_atual
@@ -77,60 +156,60 @@ def update_graph(n_clicks):
     global y_original
     global x_removidos
     global y_removidos
+    global xs_triangulo
+    global ys_triangulo
+    global triangulo_atual
     fig = go.Figure()
     fig.update_layout(width=2400, height=1200)
     if n_clicks > 0 and vertice_atual < len(estados_cortados) - 1:
-        
         if n_clicks % 2 == 1:
-            x_atual = [x[0] for x in estados_cortados[vertice_atual]] + [estados_cortados[vertice_atual][0][0]]
-            y_atual = [x[1] for x in estados_cortados[vertice_atual]] + [estados_cortados[vertice_atual][0][1]]
-            vertice_seguinte = vertice_atual + 1 if vertice_atual < len(estados_cortados) -1  else 0
-            x_proximo = [x[0] for x in estados_cortados[vertice_seguinte]] + [estados_cortados[vertice_seguinte][0][0]]
-            y_proximo = [x[1] for x in estados_cortados[vertice_seguinte]] + [estados_cortados[vertice_seguinte][0][1]]
-
-            
-            main_trace = go.Scatter(x=x_atual, y=y_atual, mode="lines+markers", name="Main Trace", line=dict(color='blue', width=2))
-            fig.add_trace(main_trace)
-
-            x_removido = encontra_elemento_removido(x_atual, x_proximo) if vertice_seguinte > 0 else x_atual[0]
-            y_removido = encontra_elemento_removido(y_atual, y_proximo) if vertice_seguinte > 0 else y_atual[0]
-    
-            indice_anterior, indice_proximo = encontra_indices_adjacentes(x_atual, x_removido)
-            x_traces = [x_atual[indice_anterior], x_removido, x_atual[indice_proximo]]
-            y_traces = [y_atual[indice_anterior], y_removido, y_atual[indice_proximo]]
-            
-            traces = go.Scatter(x=x_traces, y=y_traces, mode="lines", line=dict(color='red', width=3))
-            fig.add_trace(traces)
-            
-            x_removidos.append(x_removido)
-            y_removidos.append(y_removido)
-            for x,y in zip(x_removidos, y_removidos):
-                fig.add_trace(go.Scatter(x=[x], y=[y], mode="markers", marker=dict(color=cor_vertice(vertices_cor[x_original.index(x)]), size=20)))
+            animation_get_ear_clip(fig, vertice_atual, estados_cortados)
         else:
             vertice_atual+=1
-            x_atual = [x[0] for x in estados_cortados[vertice_atual]] + [estados_cortados[vertice_atual][0][0]]
-            y_atual = [x[1] for x in estados_cortados[vertice_atual]] + [estados_cortados[vertice_atual][0][1]]
-            vertice_seguinte = vertice_atual + 1 if vertice_atual < len(estados_cortados) -1  else 0
-            x_proximo = [x[0] for x in estados_cortados[vertice_seguinte]] + [estados_cortados[vertice_seguinte][0][0]]
-            y_proximo = [x[1] for x in estados_cortados[vertice_seguinte]] + [estados_cortados[vertice_seguinte][0][1]]
+            animation_new_graph_after_ear_clip(fig, vertice_atual, estados_cortados)
 
-            
-            main_trace = go.Scatter(x=x_atual, y=y_atual, mode="lines+markers", name="Main Trace", line=dict(color='blue', width=2))
-            fig.add_trace(main_trace)
-            for x,y in zip(x_removidos, y_removidos):
-                fig.add_trace(go.Scatter(x=[x], y=[y], mode="markers", marker=dict(color=cor_vertice(vertices_cor[x_original.index(x)]), size=18)))
-
-        # Configura transições suaves para o gráfico
-        fig.update_layout(transition={'duration': 500, 'easing': 'cubic-in-out'})
-
-        
         return fig
     elif n_clicks == 0:
         return inicia_o_grafico()
-    else:
+    elif triangulo_atual < len(xs_triangulo) -1:
+        animation_colors(fig, xs_triangulo, ys_triangulo, triangulo_atual)
+        triangulo_atual+=1
+        return fig
+    
 
-        for x,y in zip(x_removidos, y_removidos):
-            fig.add_trace(go.Scatter(x=[x], y=[y], mode="markers", marker=dict(color=cor_vertice(vertices_cor[x_original.index(x)]), size=10)))
+
+@app.callback(
+    Output('live-graph', 'figure'),
+    [Input('back-button', 'n_clicks')]
+)
+def update_graph_back(n_clicks):
+    global vertice_atual
+    global vertices_cor
+    global x_original
+    global y_original
+    global x_removidos
+    global y_removidos
+    global xs_triangulo
+    global ys_triangulo
+    global triangulo_atual
+    fig = go.Figure()
+    fig.update_layout(width=2400, height=1200)
+    if n_clicks > 0 and triangulo_atual > 0:
+        triangulo_atual-=1
+        xs_triangulo.pop()
+        ys_triangulo.pop()
+        animation_colors(fig, xs_triangulo, ys_triangulo, triangulo_atual)
+        return fig
+    elif  vertice_atual > 0:
+        if n_clicks % 2 == 1:
+            vertice_atual-=1
+            animation_new_graph_after_ear_clip(fig, vertice_atual, estados_cortados)
+            
+        else:
+            animation_get_ear_clip(fig, vertice_atual, estados_cortados)
+        return fig
+    else:
+        return inicia_o_grafico()
 
 
 # Executa o aplicativo
